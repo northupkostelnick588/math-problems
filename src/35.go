09078b0b6a8aa71d6d09d898a12881e6a24a8adb
northupkostelnick588 <northupkostelnick588@gmail.com>
2025-04-27 02:20:54 +0000

    Add 35.go

---
+++ src/35.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var number1 int = 5;
+    var number2 int = 3;
+
+    if (number1 > number2) {
+        fmt.Println("The first number is greater than the second number.")
+    } else {
+        fmt.Println("The first number is not greater than the second number.")
+    }
+}
