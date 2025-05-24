6b9ae464860c7cadda330478b2e5ac96048a6aff
northupkostelnick588 <northupkostelnick588@gmail.com>
2025-05-24 18:25:57 +0000

    Add 41.go

---
+++ src/41.go
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let max = arr[0];
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return [max, min];
+}
